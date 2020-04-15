b07d70734a730b833d85f747ff3483754e2e5a24
Brandon K. Waltz <bkwaltz@colby.edu>
2020-04-15 14:52:47 -0400

    oops

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 import Notification from '@colbycommunications/colby-notification';
 
-const responseErrorInterceptor = error => {
-    if (error && error.response) {
-        // Do something with response error
-        switch (error.response.status) {
-            case 404:
-                Notification({
-                    message: `${error.response.status}: Route Not Found`,
-                    type: 'danger',
-                    duration: 10000,
-                });
-                break;
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error && error.response) {
+            // Do something with response error
+            switch (error.response.status) {
+                case 404:
+                    Notification({
+                        message: `${error.response.status}: Route Not Found`,
+                        type: 'danger',
+                        duration: 10000,
+                    });
+                    break;
 
-            default:
-                Notification({
-                    message: `${error.response.status} : ${error.message}`,
-                    type: 'danger',
-                    duration: 10000,
-                });
-                break;
+                default:
+                    Notification({
+                        message: `${error.response.status} : ${error.message}`,
+                        type: 'danger',
+                        duration: 10000,
+                    });
+                    break;
+            }
         }
+        return Promise.reject(error);
     }
-
-    return Promise.reject(error);
-};
-
-axios.interceptors.response.use(() => {}, responseErrorInterceptor);
+);
 
 export default axios;
